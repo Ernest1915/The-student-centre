f7e9d805310b0273bb8ddeb924e65a6abfce786c
Yona Paul <ypaul6764@gmail.com>
2024-11-13 14:02:58 +0300

    Login and Signup Form Started

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/toaster";
+
+import Home from "./_root/pages/Home.tsx";
+import Layout from "./_auth/Layout.tsx";
+import LoginForm from "./_auth/forms/LoginForm.tsx";
+import SignUpForm from "./_auth/forms/SignUpForm.tsx";
+import RootLayout from "./_root/RootLayout.tsx";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<Layout />}>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      <Toaster />
+    </main>
+  );
 };
 
 export default App;

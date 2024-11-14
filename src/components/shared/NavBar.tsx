7482edc5fda9a6f8cc3e8390ea5971a27e205959
Ernest1915 <ernest.okiror@strathmore.edu>
2024-11-14 13:18:49 +0300

    new_changes

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -1,21 +1,36 @@
 import { Link, useLocation } from "react-router-dom";
 import {
-  FaShoppingCart,
-  FaRocketchat,
-  FaPeopleCarry,
-  FaAddressCard,
-  FaCog,
-  FaSignOutAlt,
-} from "react-icons/fa";
+  MdHome,
+  MdHotel,
+  MdRestaurant,
+  MdSearch,
+  MdLogout,
+  MdMenu,
+} from "react-icons/md";
+import { useState } from "react";
 import { IconType } from "react-icons/lib";
 
 const NavBar = () => {
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="newsidebar">
-      <div className="flex flex-col items-center justify-between h-full">
-        <Link to="/" className="flex gap-3 items-center">
+    <nav className="newsidebar bg-blue-500 fixed top-0 left-0 h-full md:w-20 lg:w-60">
+      {/* Toggle button for mobile */}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="absolute top-4 left-4 text-white md:hidden"
+      >
+        <MdMenu size={30} />
+      </button>
+
+      {/* Sidebar content */}
+      <div
+        className={`flex flex-col items-center justify-between h-full transition-transform duration-300 ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } md:translate-x-0`}
+      >
+        <Link to="/" className="flex gap-3 items-center mt-4">
           <img
             src="/assets/icons/logo.svg"
             alt="logo"
@@ -25,25 +40,25 @@ const NavBar = () => {
         </Link>
 
         {/* Main section icons */}
-        <div className="flex flex-col gap-4 flex-1 items-center">
-          <ul className="flex flex-col gap-4 mt-4">
+        <div className="flex flex-col gap-4 flex-1 items-center mt-8">
+          <ul className="flex flex-col gap-4">
             <Icon
-              icon={FaAddressCard}
+              icon={MdHome}
               to="/profile_a"
               currentPath={location.pathname}
             />
             <Icon
-              icon={FaPeopleCarry}
+              icon={MdHotel}
               to="/team_a"
               currentPath={location.pathname}
             />
             <Icon
-              icon={FaShoppingCart}
+              icon={MdRestaurant}
               to="/cart_a"
               currentPath={location.pathname}
             />
             <Icon
-              icon={FaRocketchat}
+              icon={MdSearch}
               to="/chat_a"
               currentPath={location.pathname}
             />
@@ -52,12 +67,7 @@ const NavBar = () => {
 
         {/* Bottom icons */}
         <div className="flex flex-col gap-4 mb-4">
-          <Icon icon={FaCog} to="/settings_a" currentPath={location.pathname} />
-          <Icon
-            icon={FaSignOutAlt}
-            to="/logout"
-            currentPath={location.pathname}
-          />
+          <Icon icon={MdLogout} to="/logout" currentPath={location.pathname} />
         </div>
       </div>
     </nav>
@@ -77,8 +87,8 @@ const Icon = ({
     <Link
       to={to}
       className={`${
-        currentPath === to ? "text-gold-dark" : "text-gray-400"
-      } hover:text-gold transition-colors duration-300`}
+        currentPath === to ? "text-blue-300" : "text-gray-400"
+      } hover:text-blue-500 transition-colors duration-300`}
     >
       <IconComponent size={30} />
     </Link>
